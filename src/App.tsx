99d637901cb4980b9f31cb9483b6c0fc4144b043
Monishobaid <monishobaid@gmail.com>
2024-09-03 21:24:46 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-red'>
+      <p className='text-3xl font-bold underline'>hello world</p>
+    </div>
+    </>
+  )
+}
+
+export default App
